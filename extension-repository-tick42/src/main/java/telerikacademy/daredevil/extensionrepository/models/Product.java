04afa8e47c2abc0ad4310dde5dee3820adc22ecf
Luchia Savova <lu.savova1@gmail.com>
2018-08-18 11:21:51 +0300

    Added models: GithubInfo, Product, Tag, User.

---
+++ extension-repository-tick42/src/main/java/telerikacademy/daredevil/extensionrepository/models/Product.java
@@ -0,0 +1,112 @@
+package telerikacademy.daredevil.extensionrepository.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String version;
+
+    private int numberOfDownloads;
+
+    @Column(nullable = false)
+    private String downloadLink;
+
+    @ManyToOne(optional = false)
+    private User owner;
+
+    @OneToOne(optional = false)
+    private GithubInfo githubInfo;
+
+    @ManyToMany
+    private List<Tag> tags;
+
+    public Product() {
+        this.tags=new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getNumberOfDownloads() {
+        return numberOfDownloads;
+    }
+
+    public void setNumberOfDownloads(int numberOfDownloads) {
+        this.numberOfDownloads = numberOfDownloads;
+    }
+
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+
+    public void setDownloadLink(String downloadLink) {
+        this.downloadLink = downloadLink;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public GithubInfo getGithubInfo() {
+        return githubInfo;
+    }
+
+    public void setGithubInfo(GithubInfo githubInfo) {
+        this.githubInfo = githubInfo;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+}
---
+++ extension-repository-tick42/src/main/java/telerikacademy/daredevil/extensionrepository/models/User.java
@@ -0,0 +1,59 @@
+package telerikacademy.daredevil.extensionrepository.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Product> products;
+
+    public User() {
+        this.products = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
