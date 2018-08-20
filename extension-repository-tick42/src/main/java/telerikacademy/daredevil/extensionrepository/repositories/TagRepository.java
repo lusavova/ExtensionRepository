dcc10c428c6c5f50e45ec2295694d246021c819a
Luchia Savova <lu.savova1@gmail.com>
2018-08-20 23:29:10 +0300

    Update repositories.

--- extension-repository-tick42/src/main/java/telerikacademy/daredevil/extensionrepository/repositories/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package telerikacademy.daredevil.extensionrepository.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import telerikacademy.daredevil.extensionrepository.models.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
--- extension-repository-tick42/src/main/java/telerikacademy/daredevil/extensionrepository/repositories/UsersRepository.java
+++ extension-repository-tick42/src/main/java/telerikacademy/daredevil/extensionrepository/repositories/UsersRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import telerikacademy.daredevil.extensionrepository.models.User;
 
 public interface UsersRepository  extends JpaRepository<User, Long> {
+    User findByUsernameAndPassword(String username,String password);
+    User findByUsername(String username);
 }
