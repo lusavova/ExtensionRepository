806cac632af6cab792d0140f04a3cc9e385a73ec
Luchia Savova <lu.savova1@gmail.com>
2018-08-23 16:31:22 +0300

    update UserRepository.

--- extension-repository/src/main/java/telerikacademy/extensionrepository/data/UserRepository.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/data/UserRepository.java
@@ -1,12 +1,20 @@
 package telerikacademy.extensionrepository.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import telerikacademy.extensionrepository.models.User;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    @Modifying
+    @Query("update  User u set u.userStatus = ?1 where  u.id = ?2")
+    void chandeUserStatus(boolean status, long id);
+
+    @Modifying
+    @Query("update  User u set u.userStatus = ?1 where  u.username = ?2")
+    void chandeUserStatus(boolean status, String username);
 }
