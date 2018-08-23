d5abc2965ff4b2e15f7aedffc94f0a9b0f709461
Luchia Savova <lu.savova1@gmail.com>
2018-08-23 19:10:33 +0300

    update Admin service

--- extension-repository/src/main/java/telerikacademy/extensionrepository/services/AdminServiceImpl.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/services/AdminServiceImpl.java
@@ -2,7 +2,9 @@ package telerikacademy.extensionrepository.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import telerikacademy.extensionrepository.data.ProductsRepository;
 import telerikacademy.extensionrepository.data.UserRepository;
+import telerikacademy.extensionrepository.models.User;
 import telerikacademy.extensionrepository.services.base.AdminService;
 
 import javax.transaction.Transactional;
@@ -11,19 +13,32 @@ import javax.transaction.Transactional;
 @Service
 public class AdminServiceImpl implements AdminService {
     private UserRepository userRepository;
+    private ProductsRepository productsRepository;
 
     @Autowired
-    public AdminServiceImpl(UserRepository userRepository) {
+    public AdminServiceImpl(UserRepository userRepository, ProductsRepository productsRepository) {
         this.userRepository = userRepository;
+        this.productsRepository = productsRepository;
     }
 
     @Override
     public void changeUserStatus(boolean status, long id) {
-        userRepository.chandeUserStatus(status, id);
+        userRepository.changeUserStatus(status, id);
     }
 
     @Override
     public void changeUserStatus(boolean status, String username) {
-        userRepository.chandeUserStatus(status, username);
+        userRepository.changeUserStatus(status, username);
+    }
+
+    @Override
+    public void approveProduct(long id) {
+        productsRepository.approveProduct(id);
+    }
+
+    @Override
+    public User getById(long id) {
+        userRepository.findById(id);
+        return null;
     }
 }
--- extension-repository/src/main/java/telerikacademy/extensionrepository/services/base/AdminService.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/services/base/AdminService.java
@@ -1,7 +1,13 @@
 package telerikacademy.extensionrepository.services.base;
 
+import telerikacademy.extensionrepository.models.User;
+
 public interface AdminService {
     void changeUserStatus(boolean status, long id);
 
     void changeUserStatus(boolean status, String username);
+
+    void approveProduct(long id);
+
+    User getById(long id);
 }
