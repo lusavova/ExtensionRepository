44b0bf5cb97108b070be84e22e9e82b929b55427
Luchia Savova <lu.savova1@gmail.com>
2018-09-01 21:29:19 +0300

    fixed file storage: save files with duplicate names.

--- extension-repository/src/main/java/telerikacademy/extensionrepository/areas/files/services/StorageServiceImpl.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/areas/files/services/StorageServiceImpl.java
@@ -17,6 +17,7 @@ import telerikacademy.extensionrepository.areas.users.models.User;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import telerikacademy.extensionrepository.areas.users.services.base.UserService;
+import telerikacademy.extensionrepository.constants.Constants;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,7 +26,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class StorageServiceImpl implements StorageService {
@@ -47,7 +52,6 @@ public class StorageServiceImpl implements StorageService {
 
     @Override
     public File getById(long id) {
-
         return fileRepository.findById(id)
                 .orElseThrow(() -> new StorageFileNotFoundException("Cannot find file with id = " + id));
     }
@@ -66,7 +70,7 @@ public class StorageServiceImpl implements StorageService {
 
     @Override
     public File store(MultipartFile multipartFile, long userId, String type) {
-        File file = null;
+        File file;
         if (multipartFile.isEmpty()) {
             throw new StorageException("Failed to store empty file.");
         }
@@ -122,10 +126,22 @@ public class StorageServiceImpl implements StorageService {
 
     private File createFile(MultipartFile multipartFile, Path path, User user, String type) {
         String filename = multipartFile.getOriginalFilename();
+        String name = filename.substring(0, filename.lastIndexOf('.'));
+
+        Set<File> files = fileRepository.findAll()
+                .stream()
+                .filter(f -> f.getFileName().equals(name))
+                .collect(Collectors.toSet());
+        if (files.size() != 0) {
+            SimpleDateFormat localDateFormat = new SimpleDateFormat(Constants.FILE_DATE_FORMAT);
+            String time = localDateFormat.format(new Date());
+            filename = time + filename;
+        }
+
         long size = multipartFile.getSize();
         String location = path.toString();
         String downloadLink = location + "\\" + filename;
-        File file = new File(filename, type , size, location, downloadLink);
+        File file = new File(filename, type, size, location, downloadLink);
         file.setOwner(user);
         return file;
     }
--- extension-repository/src/main/java/telerikacademy/extensionrepository/constants/Constants.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/constants/Constants.java
@@ -1,5 +1,5 @@
 package telerikacademy.extensionrepository.constants;
 
 public class Constants {
-    public static final String IMAGE_FOLDER_NAME = "images";
+    public static final String FILE_DATE_FORMAT = "yyyy_MM_dd_HH_mm_ss";
 }
