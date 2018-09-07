0835008aa33d729ace0ab054df370ac4f8e161d9
Luchia Savova <lu.savova1@gmail.com>
2018-09-07 09:50:44 +0300

    Added working search.

--- extension-repository/src/main/java/telerikacademy/extensionrepository/web/SearchController.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/web/SearchController.java
@@ -1,15 +1,48 @@
 package telerikacademy.extensionrepository.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import telerikacademy.extensionrepository.areas.products.controllers.ProductsController;
 import telerikacademy.extensionrepository.areas.products.models.Product;
+import telerikacademy.extensionrepository.areas.products.services.base.ProductService;
+import telerikacademy.extensionrepository.areas.tags.models.Tag;
 
+import java.sql.Struct;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class SearchController {
-//
-//    @GetMapping("/search")
-//    List<Product> products (){
-//    }
+    private ProductService productService;
+
+    @Autowired
+    public SearchController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/search/{text}")
+    @ResponseBody
+    public List<Product> products(@PathVariable String text) {
+        String searchText = text.toLowerCase();
+
+        List<Product> products = productService
+                .listAllProducts()
+                .stream()
+                .filter(pr -> {
+
+                    String desc = pr.getDescription().toLowerCase();
+                    String name = pr.getName().toLowerCase();
+
+                    Set<String> tags = pr.getTags().stream()
+                            .map(Tag::getTagname)
+                            .map(String::toLowerCase)
+                            .collect(Collectors.toSet());
+
+                    return name.contains(searchText) || desc.contains(searchText) || tags.contains(searchText);
+                })
+                .collect(Collectors.toList());
+
+        return products;
+    }
 }
