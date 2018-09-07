9075e890a3c5c70bd78acd36fbcb26fd8f816ed6
Luchia Savova <lu.savova1@gmail.com>
2018-09-07 12:43:18 +0300

    added WebConfig and DefaultExeptionHandler

--- extension-repository/src/main/java/telerikacademy/extensionrepository/web/DefaultExeptionHandler.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/web/DefaultExeptionHandler.java
@@ -1,13 +1,11 @@
-//package telerikacademy.extensionrepository.web;
-//
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//@ControllerAdvice
-//public class DefaultExeptionHandler {
-//
+package telerikacademy.extensionrepository.web;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class DefaultExeptionHandler {
 //    @ExceptionHandler(Exception.class)
-//    public String defaultGlobalExeptions(){
+//    public String defaultGlobalExeptions() {
 //        return "Ooops Something Went Wrong!";
 //    }
-//}
+}
