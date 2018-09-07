f06342047b125ab6b43e6dfdba605689179c0dea
Luchia Savova <lu.savova1@gmail.com>
2018-09-07 21:54:19 +0300

    Added exception logging

--- extension-repository/src/main/java/telerikacademy/extensionrepository/exceptions/ExceptionController.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/exceptions/ExceptionController.java
@@ -1,5 +1,7 @@
 package telerikacademy.extensionrepository.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,71 +23,74 @@ import java.util.Date;
 @ControllerAdvice
 @RestController
 public class ExceptionController extends ResponseEntityExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(ExceptionController.class);
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
                 request.getDescription(false));
+        log.error(ex.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    @ExceptionHandler(ProductNotFoundExeption.class)
-    public final ResponseEntity<ErrorDetails> handleProductNotFoundExeption(ProductNotFoundExeption ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(StorageException.class)
-    public final ResponseEntity<ErrorDetails> handleStorageException(StorageException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public final ResponseEntity<ErrorDetails> handleStorageFileNotFound(StorageFileNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public final ResponseEntity<ErrorDetails> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(FormatExeption.class)
-    public final ResponseEntity<ErrorDetails> handleFormatExeption(FormatExeption ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MalformedURLException.class)
-    public final ResponseEntity<ErrorDetails> handleMalformedURLException(MalformedURLException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(NoSuchFileException.class)
-    public final ResponseEntity<ErrorDetails> handleNoSuchFileException(NoSuchFileException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(IOException.class)
-    public final ResponseEntity<ErrorDetails> handleIOException(IOException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(TagNotFoundExeption.class)
-    public final ResponseEntity<ErrorDetails> handleTagNotFoundExeption(TagNotFoundExeption ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(UserNotFoundExeption.class)
-    public final ResponseEntity<ErrorDetails> handlecatchUserNotFoundExeption(UserNotFoundExeption ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
+//
+//    @ExceptionHandler(ProductNotFoundExeption.class)
+//    public final ResponseEntity<ErrorDetails> handleProductNotFoundExeption(ProductNotFoundExeption ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        log.error(ex.getMessage());
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(StorageException.class)
+//    public final ResponseEntity<ErrorDetails> handleStorageException(StorageException ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler(StorageFileNotFoundException.class)
+//    public final ResponseEntity<ErrorDetails> handleStorageFileNotFound(StorageFileNotFoundException ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public final ResponseEntity<ErrorDetails> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler(FormatExeption.class)
+//    public final ResponseEntity<ErrorDetails> handleFormatExeption(FormatExeption ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler(MalformedURLException.class)
+//    public final ResponseEntity<ErrorDetails> handleMalformedURLException(MalformedURLException ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler(NoSuchFileException.class)
+//    public final ResponseEntity<ErrorDetails> handleNoSuchFileException(NoSuchFileException ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(IOException.class)
+//    public final ResponseEntity<ErrorDetails> handleIOException(IOException ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler(TagNotFoundExeption.class)
+//    public final ResponseEntity<ErrorDetails> handleTagNotFoundExeption(TagNotFoundExeption ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(UserNotFoundExeption.class)
+//    public final ResponseEntity<ErrorDetails> handlecatchUserNotFoundExeption(UserNotFoundExeption ex, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
 }
