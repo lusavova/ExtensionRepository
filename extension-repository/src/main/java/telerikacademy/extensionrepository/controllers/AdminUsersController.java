efda734f966807ef1814e8fc7a9697ee1700d34e
Luchia Savova <lu.savova1@gmail.com>
2018-09-10 00:10:08 +0300

    added new method: listAllUserProducts in AdminUsersController

--- extension-repository/src/main/java/telerikacademy/extensionrepository/controllers/AdminUsersController.java
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/controllers/AdminUsersController.java
@@ -3,20 +3,26 @@ package telerikacademy.extensionrepository.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import telerikacademy.extensionrepository.enums.Roles;
+import telerikacademy.extensionrepository.models.Product;
 import telerikacademy.extensionrepository.models.User;
+import telerikacademy.extensionrepository.services.base.ProductService;
 import telerikacademy.extensionrepository.services.base.UserService;
 import telerikacademy.extensionrepository.enums.UserStatus;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin/users")
 public class AdminUsersController {
     private UserService userService;
+    private ProductService productService;
 
     @Autowired
-    public AdminUsersController(UserService userService) {
+    public AdminUsersController(UserService userService,
+                                ProductService productService) {
         this.userService = userService;
+        this.productService = productService;
     }
 
     @GetMapping("/listAll")
@@ -50,5 +56,13 @@ public class AdminUsersController {
     @ResponseBody
     public void makeUserAdmin(@PathVariable("id") long id) {
         userService.changeUserRole(id, Roles.ADMIN.name());
+    }
+
+    @GetMapping("/listAllUserProducts/{id}")
+    public List<Product> listAllUserProducts(@PathVariable long id) {
+        return productService.listAllProducts()
+                .stream()
+                .filter(p->p.getOwner().getId() == id)
+                .collect(Collectors.toList());
     }
 }
