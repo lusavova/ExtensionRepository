94d76b4c825a944501c55d50ece5f5a87431b49c
Luchia Savova <lu.savova1@gmail.com>
2018-09-10 11:24:34 +0300

    Added new IllegalOperationException.

---
+++ extension-repository/src/main/java/telerikacademy/extensionrepository/exceptions/IllegalOperationException.java
@@ -0,0 +1,7 @@
+package telerikacademy.extensionrepository.exceptions;
+
+public class IllegalOperationException extends RuntimeException {
+    public IllegalOperationException(String message) {
+        super(message);
+    }
+}
